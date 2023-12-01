dc1671293e8f4a5b804d75060de4f1130f1d6c1a
Auchitya Ray <auchityar21@gmail.com>
2023-12-01 23:29:16 +0530

    Font Changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,9 @@ import './globals.css'
 import Header from '@/components/header/page'
 import Footer from '@/components/footer/page'
 import { Nova_Square } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
-const NovaSquare = Nova_Square({ subsets: ['latin'], weight:"400" })
+const NovaSquare = Montserrat({ subsets: ['latin'], weight:"400" })
 export const metadata = {
   title: 'Tapri',
   description: 'The Indo Chinese Kitchen',
